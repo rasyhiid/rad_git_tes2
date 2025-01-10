81417c4c669a4f54f41c004185d5859e37883bcf
Radea Shiddiq <radea.sidik@gmail.com>
2025-01-10 10:25:51 +0700

    Menambahkan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Coba git remote locale_get_display_language</title>
+</head>
+<body>
+    <h1>coba git lagi</h1>
+    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veniam, voluptates voluptatibus rem provident consequatur dolor quod necessitatibus expedita officia tenetur quibusdam! Asperiores quasi maiores repellendus eum sint, quam soluta! Quae!</p>
+</body>
+</html>
